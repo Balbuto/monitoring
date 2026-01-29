2cae5b60432c2b451a8a3b790b26ef87167a5d2d
Balbuto <47351177+Balbuto@users.noreply.github.com>
2026-01-29 10:21:13 +1000

    Update fonts.ts

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,11 +1,11 @@
 import { JetBrains_Mono as FontMono, Noto_Sans_SC as FontSans } from 'next/font/google';
 
 export const fontSans = FontSans({
-  subsets: ['latin'],
+  subsets: ['CyrE'],
   variable: '--font-sans',
 });
 
 export const fontMono = FontMono({
-  subsets: ['latin'],
+  subsets: ['CyrE'],
   variable: '--font-mono',
 });
